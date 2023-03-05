e9a2fb942144a035e7d95c1a7b759cb3d4a6ad0b
Braden <bradenoptimus@gmail.com>
2023-03-05 11:44:36 -0700

    preperatory js for calendar page

--- calendar.js
+++ calendar.js
@@ -1,5 +1,20 @@
 let calDB = [];
 
+function getCalDays() {
+    thing = new Date();
+    return new Date(thing.getFullYear(), thing.getMonth()+1,0).getDate();
+}
+
+function getOffSet(){
+    thing = new Date();
+    thang = new Date(thing.getFullYear(), thing.getMonth()-1,1);
+    console.log(thang);
+    return thang.getDay();
+}
+
+function logDays() {
+    console.log(getOffSet());
+}
 /*
 Calendar:
  - add events (on clicking on day) [dialog]
