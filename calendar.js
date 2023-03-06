8d020cf61ded6767e4cd0810e133bd3c1ffb5883
Braden <bradenoptimus@gmail.com>
2023-03-06 16:34:01 -0700

    work on daily and weekly calendars

--- calendar.js
+++ calendar.js
@@ -15,21 +15,106 @@ function getOffSet(){
 
 function generateCalendar() {
     if (type === "Day") {
-        // get calendar holder
         const cal = document.querySelector('#cal');
         cal.innerHTML = "";
-        // create day
 
-        // populate events
+        const dayHead = document.createElement('div');
+        dayHead.innerText = "3/6/23";
+        dayHead.className = "week-head";
+        cal.appendChild(dayHead);
+
+        // create week
+        const DayCal = document.createElement('div');
+        DayCal.className = "week-cal";
+
+        const legchunk = document.createElement('div');
+        legchunk.className = "week-day";
+
+        for (let i = 0; i < 15; i++) {
+            if (i === 6) {
+                const legend = document.createElement('div');
+                legend.innerText = '12 pm';
+                legend.className = "hours";
+                legchunk.appendChild(legend);
+            } else {
+                const legend = document.createElement('div');
+                const mer = i < 6 ? "am" : "pm";
+                legend.innerText = `${(i + 6) % 12} ${mer}`;
+                legend.className = "hours";
+                legchunk.appendChild(legend);
+            }
+        }
+        DayCal.appendChild(legchunk);
+
+        const column = document.createElement('div');
+        column.className = "day-day";
+        for (let i = 0; i < 14; i++) {
+            const chunk = document.createElement('div');
+            chunk.className = "hour";
+            column.appendChild(chunk);
+        }
+        const chunk = document.createElement('div');
+        chunk.className = "last-hour";
+        column.appendChild(chunk);
+        DayCal.appendChild(column);
+
+        cal.appendChild(DayCal);
 
     } else if (type === "Week") {
         // get calendar holder
         const cal = document.querySelector('#cal');
         cal.innerHTML = "";
 
+        const weekHead = document.createElement('div');
+        weekHead.innerText = "3/6/23 - 3/12/23";
+        weekHead.className = "week-head";
+        cal.appendChild(weekHead);
+
         // create week
         const WeekCal = document.createElement('div');
         WeekCal.className = "week-cal";
+
+        const space = document.createElement('div');
+        WeekCal.appendChild(space);
+        for (let i = 0; i < 7; i++) {
+            const Weekday = document.createElement('div');
+            Weekday.className = "weekday week-weekday";
+            Weekday.innerText = days[i];
+            WeekCal.appendChild(Weekday);
+        }
+
+        const legchunk = document.createElement('div');
+        legchunk.className = "week-day";
+
+        for (let i = 0; i < 15; i++) {
+            if (i === 6) {
+                const legend = document.createElement('div');
+                legend.innerText = '12 pm';
+                legend.className = "hours";
+                legchunk.appendChild(legend);
+            } else {
+                const legend = document.createElement('div');
+                const mer = i < 6 ? "am" : "pm";
+                legend.innerText = `${(i + 6) % 12} ${mer}`;
+                legend.className = "hours";
+                legchunk.appendChild(legend);
+            }
+        }
+        WeekCal.appendChild(legchunk);
+
+        for (let i = 0; i < 7; i++) {
+            const column = document.createElement('div');
+            column.className = "week-day";
+            for (let i = 0; i < 14; i++) {
+                const chunk = document.createElement('div');
+                chunk.className = "hour";
+                column.appendChild(chunk);
+            }
+            const chunk = document.createElement('div');
+            chunk.className = "last-hour";
+            column.appendChild(chunk);
+            WeekCal.appendChild(column);
+        }
 
         cal.appendChild(WeekCal);
         // populate events
