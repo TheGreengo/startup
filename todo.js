e01981d8b40003d4346901fb13dd69001deae011
Braden <bradenoptimus@gmail.com>
2023-03-06 23:15:27 -0700

    Initial todos setup

--- todo.js
+++ todo.js
@@ -1,4 +1,73 @@
-let todoDB = [];
+let todoDB = [
+  {
+    description: "Task 1",
+    dueDate: new Date("3-14-2023"),
+    type: ["important","personal"]
+  },
+  {
+    description: "Task 2",
+    dueDate: new Date("3-15-2023"),
+    type: ["important","dull"]
+  },
+  {
+    description: "Task 3",
+    dueDate: new Date("3-16-2023"),
+    type: ["dull","personal"]
+  }
+];
+
+types = ["important","personal","dull"];
+
+function createTodos() {
+  const holder = document.querySelector('#display');
+  holder.inneHTML = "";
+
+  for (let i = 0; i < todoDB.length; i++) {
+    const newTask = document.createElement('div');
+    newTask.className = "task";
+
+    const compBtn = document.createElement('button');
+    compBtn.textContent = `${String.fromCodePoint(10004)}`;
+    compBtn.className = "comp-btn";
+    newTask.appendChild(compBtn);
+
+    const editBtn = document.createElement('button');
+    editBtn.textContent = "Edit";
+    editBtn.className = "edit-btn";
+    newTask.appendChild(editBtn);
+
+    const taskMN = document.createElement('div');
+    taskMN.className = "task-mn";
+
+    const desc = document.createElement('div');
+    desc.innerText = `Description: ${todoDB[i].description}`;
+
+    const typ = document.createElement('div');
+    let thing = "";
+    for (let j = 0; j < todoDB[i].type.length; j++) {
+      thing += todoDB[i].type[j];
+      if (j < todoDB[i].type.length - 1) {
+        thing += ", ";
+      }
+    }
+    typ.innerText = `Type: ${thing}`;
+
+    const dt = document.createElement('div');
+    dt.innerText = `Date: ${todoDB[i].dueDate.getMonth()+1}-${todoDB[i].dueDate.getDate()}-${todoDB[i].dueDate.getFullYear() - 2000}`;
+
+    taskMN.appendChild(desc);
+    taskMN.appendChild(typ);
+    taskMN.appendChild(dt);
+    newTask.appendChild(taskMN);
+
+    const delBtn = document.createElement('button');
+    delBtn.textContent = `${String.fromCodePoint(10006)}`;
+    delBtn.className = "dlt-btn";
+    newTask.appendChild(delBtn);
+
+    holder.appendChild(newTask);
+  }
+}
 
 /*
 Todo:
@@ -15,3 +84,5 @@ Todo:
  - fix sort options
  - sort
 */
+
+createTodos();
