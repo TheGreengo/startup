6a03bc4d489010d2bd6a7eb7a06a6ae92d77cb5d
Braden <bradenoptimus@gmail.com>
2023-04-05 17:41:43 -0600

    database basics added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
@@ -11,14 +12,16 @@ const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
 // Here is where the individual routes will go
-// apiRouter.get('/scores', (_req, res) => {
-//     res.send(scores);
+// apiRouter.get('/scores', async (_req, res) => {
+//  const scores = await DB.getHighScores();
+//  res.send(scores);
 // });
   
-// apiRouter.post('/score', (req, res) => {
-//     scores = updateScores(req.body, scores);
+// apiRouter.post('/score', async (req, res) => {
+//     DB.addScore(req.body);
+//     const scores = await DB.getHighScores();
 //     res.send(scores);
-// });
+//   });
 
 app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
